1df38428dfd4f377f4fbf2f4ede23f6feb24049c
DeYao Chen <1348323271@qq.com>
2023-05-06 00:01:12 +0800

    update ToolBar

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="mt-2 flex min-w-max">
+    <ToolBarItem @click="handleTool" disabled>
+      <button class="text-2xl font-bold">✄</button>
+      <template #prompt>
+        <p>Cut Traces</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">©</button>
+      <template #prompt>
+        <p>Copy Traces</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">℗</button>
+      <template #prompt>
+        <p>Paste Traces</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">√</button>
+      <template #prompt>
+        <p>Zoom Fit</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="font-bold">➕</button>
+      <template #prompt>
+        <p>Zoom In</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="font-bold ">➖</button>
+      <template #prompt>
+        <p>Zoom Out</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">⎌</button>
+      <template #prompt>
+        <p>Zoom Undo</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">⏪</button>
+      <template #prompt>
+        <p>Zoom to Start</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">⏩</button>
+      <template #prompt>
+        <p>Zoom to End</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">◀</button>
+      <template #prompt>
+        <p>Find Previous Edge</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">▶</button>
+      <template #prompt>
+        <p>Find Next Edge</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool" class="min-w-max ">
+      <label class="ml-2 pr-2">
+        <span class=" ">From:</span>
+        <input
+          class="ml-1 w-20 rounded-sm pl-2"
+          type="text"
+          placeholder="0 sec"
+        />
+      </label>
+      <template #prompt>
+        <p>Scroll Lower Bound(use MX for named marker X)</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool" class="min-w-max " disabled>
+      <label class="ml-2 pr-2">
+        <span class="">To:</span>
+        <input
+          class="ml-1 w-20 rounded-sm pl-2 "
+          type="text"
+          placeholder="0 sec"
+        />
+      </label>
+      <template #prompt>
+        <p>Scroll Upper Bound(use MX for named marker X)</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool">
+      <button class="text-2xl font-bold">🔃</button>
+      <template #prompt>
+        <p>Reload</p>
+      </template>
+    </ToolBarItem>
+    <ToolBarItem @click="handleTool" class="min-w-max px-3">
+      <span>Marker: | Cursor: 0 sec</span>
+    </ToolBarItem>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import ToolBarItem from './ToolBarItem.vue';
+
+const handleTool = ()=>{
+    console.log(111);
+}
+</script>
+
+<style scoped></style>
