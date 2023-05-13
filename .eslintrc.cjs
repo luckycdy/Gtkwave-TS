5f85fcafec1645ad35cfbe4871131dee693a3371
DeYao Chen <1348323271@qq.com>
2023-05-13 20:49:14 +0800

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,25 @@
-/* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
   root: true,
-  'extends': [
-    "plugin:prettier/recommended",
-    "eslint-config-prettier",
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  "plugins": [
-    "prettier"
+    '@vue/eslint-config-typescript/recommended',
+    'plugin:vue/vue3-recommended',
+    // 'plugin:prettier/recommended',
+    // '@vue/eslint-config-prettier',
+    // '@vue/eslint-config-prettier/skip-formatting',
+    'eslint-config-prettier',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 }
